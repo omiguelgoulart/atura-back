229e3a64fc8f4baf8b51e90ddce60b36b0678602
Luis Miguel Goulart <luismiguelgoulart@gmail.com>
2025-05-22 11:40:01 -0300

    update carrinho

---
+++ prisma/migrations/20250522143938_update_carrinho/migration.sql
@@ -0,0 +1,142 @@
+-- CreateEnum
+CREATE TYPE "STATUS_PEDIDO" AS ENUM ('PENDENTE', 'PROCESSANDO', 'ENVIADO', 'ENTREGUE', 'CANCELADO');
+
+-- CreateEnum
+CREATE TYPE "STATUS" AS ENUM ('PENDENTE', 'PROCESSANDO', 'ENVIADO', 'ENTREGUE', 'CANCELADO');
+
+-- CreateEnum
+CREATE TYPE "STATUS_ITEM" AS ENUM ('CARRINHO', 'PEDIDO', 'FINALIZADO');
+
+-- CreateTable
+CREATE TABLE "marca" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+
+    CONSTRAINT "marca_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "produto" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "preco" DOUBLE PRECISION NOT NULL,
+    "categoria" TEXT NOT NULL,
+    "estoque" INTEGER NOT NULL,
+    "volumeMl" INTEGER NOT NULL DEFAULT 0,
+    "marca_id" INTEGER NOT NULL,
+    "foto" TEXT NOT NULL,
+
+    CONSTRAINT "produto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fotos" (
+    "id" SERIAL NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "produtoId" INTEGER NOT NULL,
+
+    CONSTRAINT "fotos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "clientes" (
+    "id" VARCHAR(36) NOT NULL,
+    "nome" VARCHAR(60) NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" VARCHAR(60) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpires" TIMESTAMP(3),
+    "telefone" VARCHAR(15),
+
+    CONSTRAINT "clientes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "enderecos" (
+    "id" SERIAL NOT NULL,
+    "rua" VARCHAR(100) NOT NULL,
+    "numero" VARCHAR(20) NOT NULL,
+    "cidade" VARCHAR(100) NOT NULL,
+    "estado" VARCHAR(50) NOT NULL,
+    "cep" VARCHAR(20) NOT NULL,
+    "complemento" VARCHAR(100),
+    "clienteId" TEXT NOT NULL,
+
+    CONSTRAINT "enderecos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pedidos" (
+    "id" SERIAL NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "status" "STATUS_PEDIDO" NOT NULL,
+    "total" DECIMAL(10,2) NOT NULL,
+    "clienteId" TEXT NOT NULL,
+    "enderecoId" INTEGER,
+
+    CONSTRAINT "pedidos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "itens_transacao" (
+    "id" SERIAL NOT NULL,
+    "status" "STATUS_ITEM" NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "preco_unitario" DECIMAL(10,2) NOT NULL,
+    "clienteId" TEXT NOT NULL,
+    "produtoId" INTEGER NOT NULL,
+    "pedidoId" INTEGER,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "atualizadoEm" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "itens_transacao_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "avaliacoes" (
+    "id" SERIAL NOT NULL,
+    "nota" INTEGER NOT NULL,
+    "comentario" VARCHAR(500),
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "clienteId" TEXT NOT NULL,
+    "produtoId" INTEGER NOT NULL,
+
+    CONSTRAINT "avaliacoes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clientes_email_key" ON "clientes"("email");
+
+-- AddForeignKey
+ALTER TABLE "produto" ADD CONSTRAINT "produto_marca_id_fkey" FOREIGN KEY ("marca_id") REFERENCES "marca"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fotos" ADD CONSTRAINT "fotos_produtoId_fkey" FOREIGN KEY ("produtoId") REFERENCES "produto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "enderecos" ADD CONSTRAINT "enderecos_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "clientes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pedidos" ADD CONSTRAINT "pedidos_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "clientes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pedidos" ADD CONSTRAINT "pedidos_enderecoId_fkey" FOREIGN KEY ("enderecoId") REFERENCES "enderecos"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "itens_transacao" ADD CONSTRAINT "itens_transacao_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "clientes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "itens_transacao" ADD CONSTRAINT "itens_transacao_produtoId_fkey" FOREIGN KEY ("produtoId") REFERENCES "produto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "itens_transacao" ADD CONSTRAINT "itens_transacao_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "pedidos"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "avaliacoes" ADD CONSTRAINT "avaliacoes_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "clientes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "avaliacoes" ADD CONSTRAINT "avaliacoes_produtoId_fkey" FOREIGN KEY ("produtoId") REFERENCES "produto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
